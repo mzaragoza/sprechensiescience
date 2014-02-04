b46cf570bba3be243de0ccf243bad347315afc54
Moises Zaragoza <mZaragoza@circletechfl.com>
2014-02-03 23:24:33 -0500

    video show page

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -1,4 +1,15 @@
 class EpisodesController < PublicController
   expose(:episodes){ Episode.order("id DESC").scoped{} }
+  expose(:episode, attributes: :episode_params)
+
+private
+  def episode_params
+    params.require(:episode).permit(
+      :name,
+      :description,
+      :video,
+      :icon
+    )
+  end
 end
 
