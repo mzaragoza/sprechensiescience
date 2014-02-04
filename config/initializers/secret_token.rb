ca9f75048b44615013701e295fc0929806fc6e27
Moises Zaragoza <mZaragoza@circletechfl.com>
2014-02-03 21:30:12 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sprechensiescience::Application.config.secret_key_base = '350458b7d427db69a185b5d430540aef627f6a044a023a1620333aa870c2bdbf5e4eaf92a5ff8ef55be477f5e895b6408d02e2a8677125515002a6b2c3a493d9'
